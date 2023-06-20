b336aa532b51075d219d0f5a88c648bd2af70de6
Amir Sabahi <amir.sabahi@gmail.com>
2023-06-20 14:06:13 +0330

    Update 2DObjectKinematicsCalculation.cs

--- 2DObjectKinematicsCalculation.cs
+++ 2DObjectKinematicsCalculation.cs
@@ -1,5 +1,11 @@
 using System;
+/**
+In this code, we define a Particle class that represents a particle in 2D space. It has properties for position (X and Y) and velocity (X and Y). 
+The UpdatePosition method calculates the new position of the particle based on its velocity and the given time.
 
+In the Main method, we create an instance of the Particle class, set its initial position and velocity, and then update its position for a specific time. 
+You can modify the time variable to see how the position changes over time.
+*/
 public class Particle
 {
     public double PositionX { get; set; }
@@ -24,7 +30,7 @@ public class Program
         particle.VelocityX = 1.5;
         particle.VelocityY = 2.0;
 
-        double time = 2.5; // Change this value to update position for a different time
+        double time = 2.5; // Change this value to update the position for a different time
 
         Console.WriteLine($"Initial Position: ({particle.PositionX}, {particle.PositionY})");
         particle.UpdatePosition(time);
