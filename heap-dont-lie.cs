97a2142078aa123955ed9e048322dc2149969381
Amir Sabahi <amir.sabahi@gmail.com>
2023-06-25 09:31:08 +0330

    Create heap-dont-lie.cs

---
+++ heap-dont-lie.cs
@@ -0,0 +1,18 @@
+using System;
+
+class HeaSort {
+
+  public static void sort<T>(T[] array) {
+    // To be implemented
+  }
+  private static void sink<T>(T[] array) {
+    // To be implemented
+  }
+  private static void swap<T>(T[] array, int heapSize, int toSinkPos) {
+    // To be implemented
+  }
+
+  private static void printArray<T> (T[] array) {
+    // To be implemented
+  }
+}
