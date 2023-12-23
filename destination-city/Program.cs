1723ed4c4d8e9ec81821b73fe5fd7cca03034d40
amirsabahi <ah.sabahi@outlook.com>
2023-12-23 19:59:50 +0330

    Add  destination city solution

---
+++ destination-city/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace distination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Destination(new List<List<string>>{new List<string>{"London","New York"},new List<string>{"New York","Lima"},new List<string>{"Lima","Sao Paulo"}}));
+        }
+
+        private static string Destination(List<List<string>> paths)
+        {
+            HashSet<string> StartCity = new HashSet<string>();
+         
+            string[] DistinationCity = new string[paths.Count];
+            int i = 0;
+            foreach(List<string> path in paths)
+            {
+                StartCity.Add(path[0]);
+                DistinationCity[i++] = path[1];
+            }
+
+            foreach(string city in DistinationCity)
+            {
+                if(!StartCity.Contains(city))
+                {
+                    return city;
+                }
+            }
+            return "";
+        }
+    }
+}
