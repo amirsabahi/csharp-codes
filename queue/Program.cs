149e54e9b971f621b860112ffce8858d58cd75fa
amirsabahi <ah.sabahi@outlook.com>
2024-02-17 10:57:22 +0330

    A simple Queue

---
+++ queue/Program.cs
@@ -0,0 +1,42 @@
+﻿
+Console.Clear();
+Console.WriteLine("Welcome to the Queue Program!");
+
+public class Queue<T>
+{
+    private T[] queue;
+    private int head = 0;
+    private int tail = 0;
+    private int size = 0;
+
+    public Queue(int size)
+    {
+        this.size = size;
+        this.queue = new T[size];
+    }
+
+    public void Enqueue(T item)
+    {
+        if tail>size
+        {
+            Console.WriteLine("Queue is full");
+            return false;
+        }
+
+        queue[tail] = item;
+        tail++;
+    }
+    public T Dequeue()
+    {
+        if head ==tail
+        {
+            Console.WriteLine("Queue is empty");
+            return false;
+        }
+        T item = queue[head]
+        head++;
+        return item;
+    }
+
+}
+
