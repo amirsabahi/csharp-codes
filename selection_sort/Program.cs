37e6152d051b89eb37ad32dfb74d3fa861123fe9
amirsabahi <ah.sabahi@outlook.com>
2024-02-26 13:24:50 +0330

    Selection sort

---
+++ selection_sort/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+    int[] sortedArray = Sort.selectionSort(new int[] { 5, 3, 4, 1, 2 });
+    foreach (int value in sortedArray)
+    {
+        Console.WriteLine(value);
+    }
+    class Sort
+    {
+        public static int[] selectionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int tempValue = array[i];
+                int position = i - 1;
+                while (position >= 0)
+                {
+                    if (array[position] > tempValue)
+                    {
+                        array[position + 1] = array[position];
+                        position--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                array[position + 1] = tempValue;
+            }
+            return array;
+        }
+    }
+
+
+
