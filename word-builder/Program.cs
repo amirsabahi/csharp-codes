0106c9ce39900fc2cda60ef012053bb1994d7cfe
amirsabahi <ah.sabahi@outlook.com>
2024-02-28 13:23:38 +0330

    Add word builder

---
+++ word-builder/Program.cs
@@ -0,0 +1,23 @@
+﻿var array = WordBuilder.Build(new string[] { "a", "b", "c", "d" });
+foreach(string word in array)
+{
+    Console.WriteLine(word);
+}
+class WordBuilder
+{
+    public static string[] Build(string[] array)
+    {
+        List<string> newArray = new List<string>();
+        for(int i=0; i<array.Length; i++)
+        {
+            for(int j=0; j<array.Length; j++)
+            {
+                if(i!=j)
+                {
+                    newArray.Add(array[i] + array[j]);
+                }
+            }
+        }    
+        return newArray.ToArray();
+    } 
+}
