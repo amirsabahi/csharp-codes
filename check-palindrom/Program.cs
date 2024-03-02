4b6700f55544b8e68c753e136d8db06495aaaf09
amirsabahi <ah.sabahi@outlook.com>
2024-03-02 20:00:58 +0330

    Palindrom

---
+++ check-palindrom/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+bool isPalinddrome(string str)
+{
+    int firstIndex = 0;
+    int lastIndex = str.Length - 1;
+    str = str.ToLower();
+    while(firstIndex < str.Length/2)
+    {
+        if(str[firstIndex] != str[lastIndex])
+        {
+            return false;
+        }
+        firstIndex++;
+        lastIndex--;
+    } 
+    return true;
+}
+
+if(isPalinddrome("Kayak"))
+{
+    Console.WriteLine("It is a palindrome");
+}
+else
+{
+    Console.WriteLine("It is not a palindrome");
+}
