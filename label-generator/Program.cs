e664df154de6f36cbf53c949c0874a68c3cf6388
amirsabahi <ah.sabahi@outlook.com>
2024-03-02 19:24:45 +0330

    Label Generator

---
+++ label-generator/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] listOfClothes = new string[] {
+    "t-shirts", "sweatshirts", "pants", "shorts", "dresses", "skirts", 
+    "blouses", "suits", "jackets", "coats", "hats", "gloves", 
+    "scarves", "socks", "shoes", "boots", "slippers", "sandals",    
+};
+
+foreach(string item in listOfClothes)
+{
+    for(int size=1; size<6; size++)
+    {
+        Console.WriteLine($"{size} {item}");
+    }
+    
+}
