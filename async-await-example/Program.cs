5ca74fb38cbd06502523cc583488843a04117b1b
amirsabahi <ah.sabahi@outlook.com>
2024-03-04 20:40:54 +0330

    Async Await simple example

---
+++ async-await-example/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+
+WriteLine("Connecting...");
+// string url = ReadLine();
+// string r = url ??= "https://www.microsoft.com/?";
+// WriteLine(r);
+
+HttpClient clinet = new();
+HttpResponseMessage response = await clinet.GetAsync("https://www.microsoft.com");
+WriteLine("Microsoft's home page has {0:N0} bytes.", response.Content.Headers.ContentLength);
