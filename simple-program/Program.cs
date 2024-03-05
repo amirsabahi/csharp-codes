a07dcfa1584b8fb0c04484206bc760d6b0aba2ed
amirsabahi <ah.sabahi@outlook.com>
2024-03-05 09:19:41 +0330

    Practice

---
+++ simple-program/Program.cs
@@ -0,0 +1,20 @@
+﻿object score = 3;
+
+if(score is int i) 
+{
+    Console.WriteLine($"The score is {i}");
+}
+else 
+{
+    Console.WriteLine("The score is not an integer");
+}
+
+string? actualPassword = "AAA";
+string? expectedPass;
+
+do{
+    WriteLine("Enter your password: ");
+    expectedPass = ReadLine();
+} while(expectedPass != actualPassword);
+
+WriteLine("Password accepted!");
