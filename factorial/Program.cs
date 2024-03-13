7e7f952a6ca68cbca414bcf40832640e916264f4
amirsabahi <ah.sabahi@outlook.com>
2024-03-13 21:36:29 +0330

    Add factorial program

---
+++ factorial/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+try
+{
+        uint result = factorial(100); 
+        WriteLine($"Factorial of 10 is {result}");
+}
+catch (ArgumentOutOfRangeException ex)
+{
+    WriteLine(ex.Message);
+}
+catch (OverflowException)
+{
+    WriteLine($"! is too big for a 32-bit integer.");
+}
+catch (Exception ex)
+{
+    WriteLine($" throws {ex.GetType()}: {ex.Message}");
+}
+
+
+static uint factorial(uint number)
+{
+    WriteLine($"Calculating factorial of {number}");
+    if (number < 0)
+    {
+        throw new ArgumentOutOfRangeException(nameof(number), "Number must be greater than or equal to 1");
+    }
+    else if (number == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        checked
+        {
+            return number * factorial(number - 1);
+        }
+    }
+}
