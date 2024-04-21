f3c749a58b8b101a1037cba7ed05ca6395cdc76b
amirsabahi <ah.sabahi@outlook.com>
2024-04-21 19:00:09 +0330

    Merge two sorted arrays

---
+++ MergeTwoArrays/Program.cs
@@ -0,0 +1,54 @@
+﻿
+int[] res = Solution.Merge(new int[] { 1, 2, 3, 10, 12, 14 }, new int[] { 2, 5, 6, 13, 14 });
+foreach (var item in res)
+{
+    Console.WriteLine(item);
+}
+class Solution
+{
+    public static int[] Merge(int[] nums1, int[] nums2)
+    {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int num1Length = nums1.Length;
+        int num2Length = nums2.Length;
+        int[] result = new int[num1Length + num2Length];
+
+        while (i < num1Length && j < num2Length)
+        {
+            if (nums1[i] < nums2[j])
+            {
+                result[k] = nums1[i];
+                i++;
+            }
+            else
+            {
+                result[k] = nums2[j];
+                j++;
+
+
+            }
+            k++;
+        }
+
+        while (i < num1Length)
+        {
+            result[k] = nums1[i];
+            i++;
+            k++;
+        }
+
+        while (j < num2Length)
+        {
+            result[k] = nums2[j];
+            j++;
+            k++;
+        }
+        return result;
+    }
+
+
+}
+
+
