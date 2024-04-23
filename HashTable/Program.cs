c32f0d3c7dc9bd156df4b01f57abb9436b6a1118
amirsabahi <ah.sabahi@outlook.com>
2024-04-23 21:17:21 +0330

    Add HashTable

---
+++ HashTable/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information∂
+
+public class HashTable
+{
+    private object[] HashTableArray;
+    public HashTable(int size)
+    {
+        HashTableArray = new object[size];
+    }
+
+    private int _Hash(string key)
+    {
+        int Hash = 0;
+        for (int i = 0; i < key.Length; i++)
+        {
+            Hash = (Hash + key[i] * i) % HashTableArray.Length;
+        }
+        return Hash;
+    }
+
+    public void set(string Key, object value)
+    {
+        HashTableArray[_Hash(Key)] = value;
+    }
+
+    public object get(string Key)
+    {
+        return HashTableArray[_Hash(Key)];
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        HashTable hashTable = new HashTable(50);
+        hashTable.set("Amir", 420);
+        object result = hashTable.get("Amir");
+
+        System.Console.WriteLine(result);
+    }
+}
